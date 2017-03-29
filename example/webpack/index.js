6948a447b1af898b73b5ee548f7a987c2b5ddfa3
nju33 <nju33.ki@gmail.com>
2017-03-30 01:31:28 +0900

    Add example/webpack

--- example/webpack/index.js
+++ example/webpack/index.js
@@ -1,1 +1,23 @@
 import ApocSidebar from '../..';
+
+(() => {
+  [
+    'slide',
+    'water',
+    'push',
+    'lid',
+    'door',
+    'waterfall',
+    'waterfallReverse'
+  ].forEach(type => {
+    const s = new ApocSidebar(document.getElementById(type), {type});
+    s.init();
+    document.getElementById(type + '-trigger').addEventListener('click', () => {
+      if (s.isOpen()) {
+        s.close();
+      } else {
+        s.open();
+      }
+    });
+  });
+})();
